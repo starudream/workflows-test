5bd8beb13b0802994524d7ae684865d41f4f7117
StarUDream <justwangsheng@qq.com>
2020-04-30 18:10:44 +0800

    .

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ echo -e "\033[34m[ Build '${NAME}' ]\033[0m"
 docker build -q -t starudream/"${NAME}":latest .
 
 echo -e "\033[32m[ Publish Docker Hub ]\033[0m"
-docker login -u starudream -p "${secrets.DOCKER_TOKEN}"
+docker login -u starudream -p "${{secrets.DOCKER_TOKEN}}"
 docker push starudream/"${NAME}":latest
 
 echo -e "\033[32m[ Publish Aliyun Docker Hub ]\033[0m"
-docker login -u "${secrets.ALIYUN_DOCKER_USERNAME}" -p "${secrets.ALIYUN_DOCKER_TOKEN}" registry.cn-hangzhou.aliyuncs.com
+docker login -u "${{secrets.ALIYUN_DOCKER_USERNAME}}" -p "${{secrets.ALIYUN_DOCKER_TOKEN}}" registry.cn-hangzhou.aliyuncs.com
 docker tag starudream/"${NAME}":latest registry.cn-shanghai.aliyuncs.com/starudream/"${NAME}":latest
 
 cd "../"
