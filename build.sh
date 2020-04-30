f4c81d01d08485c931de3b8dceda162b484de2f0
StarUDream <justwangsheng@qq.com>
2020-04-30 18:32:51 +0800

    .

--- build.sh
+++ build.sh
@@ -4,16 +4,21 @@ NAME=$1
 
 cd "./${NAME}" || exit 1
 
-echo -e "\033[34m[ Build '${NAME}' ]\033[0m"
+echo -e "\033[34m[ Start '${NAME}' ]\033[0m"
+
 docker build -q -t starudream/"${NAME}":latest .
 
+echo -e "\033[32m[ Login Docker Hub ]\033[0m"
+docker login -u starudream -p "${DOCKER_TOKEN}"
 echo -e "\033[32m[ Publish Docker Hub ]\033[0m"
-docker login -u starudream -p "${DOCKER_TOKEN}" 1>/dev/null
-docker push starudream/"${NAME}":latest 1>/dev/null
+docker push starudream/"${NAME}":latest
 
-echo -e "\033[32m[ Publish Aliyun Docker Hub ]\033[0m"
+echo -e "\033[32m[ Login Aliyun Docker ]\033[0m"
 docker login -u "${ALIYUN_DOCKER_USERNAME}" -p "${ALIYUN_DOCKER_TOKEN}" registry.cn-hangzhou.aliyuncs.com 1>/dev/null
+echo -e "\033[32m[ Publish Aliyun Docker ]\033[0m"
 docker tag starudream/"${NAME}":latest registry.cn-shanghai.aliyuncs.com/starudream/"${NAME}":latest 1>/dev/null
 docker push registry.cn-shanghai.aliyuncs.com/starudream/"${NAME}":latest
+
+echo -e "\033[34m[ End '${NAME}' ]\033[0m"
 
 cd "../"
